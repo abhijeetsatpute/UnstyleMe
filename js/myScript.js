9677955851743be4b01cb05596f636c617da4cfc
Abhijeet Satpute <abhi111sat@gmail.com>
2020-05-20 09:12:03 +0530

    Adding cards

--- js/myScript.js
+++ js/myScript.js
@@ -1,25 +1,15 @@
 var btnBg1 = document.getElementById("changeBg1");
-var btnBg2 = document.getElementById("changeBg2");
 var whiteBtn = document.getElementById("whiteBg");
 var closebtn = document.getElementById("closebtn");
 
 function changeBackground1(){
     document.body.style.backgroundImage = "url('images/bg2.jpg')";
     btnBg1.setAttribute("class","btn btn-success");
-    btnBg2.setAttribute("class","btn btn-secondary");
-    whiteBtn.setAttribute("class","btn btn-secondary");
-}
-
-function changeBackground2(){
-    document.body.style.backgroundImage = "url('images/bg1.jpg')";
-    btnBg2.setAttribute("class","btn btn-success");
-    btnBg1.setAttribute("class","btn btn-secondary");
     whiteBtn.setAttribute("class","btn btn-secondary");
 }
 
 function whiteBackground(){
     document.body.style.backgroundImage = "none";
-    btnBg2.setAttribute("class","btn btn-secondary");
     btnBg1.setAttribute("class","btn btn-secondary");
     whiteBtn.setAttribute("class","btn btn-success");
 }
@@ -32,7 +22,6 @@ function closeWindow(){
 }
 
 btnBg1.addEventListener("click",changeBackground1);
-btnBg2.addEventListener("click",changeBackground2);
 whiteBtn.addEventListener("click",whiteBackground);
 closebtn.addEventListener("click",closeWindow);
 
